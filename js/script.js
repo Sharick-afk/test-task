e88c8328ddd67d8761b3f9a8dc6d67a0ed924f4c
Sharic <68066232+Sharick-afk@users.noreply.github.com>
2020-12-23 14:33:58 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready((function(){
+  $(".burger_box ,.burger_span").click((function(s){
+    $(".head_nav,.head_logo_img,.burger_box,.burger_span,.buger_box").toggleClass("active");
+
+  }))}));
